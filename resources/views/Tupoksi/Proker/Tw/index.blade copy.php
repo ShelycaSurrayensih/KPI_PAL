3e5879ee22866a3a27f7c4d5ce2d18c4fc8dd534
RadithyaIP <radithyaiqbalprasaja@gmail.com>
2022-09-21 16:00:56 +0700

    tupoksi

--- resources/views/Tupoksi/Proker/Tw/index.blade copy.php
+++ resources/views/Tupoksi/Proker/Tw/index.blade copy.php
@@ -2,14 +2,19 @@
 
 @section('content')
 <div class="row">
-    <div class="col-xl-12">
+    <div class="col-lg-12">
         <div class="card">
             <div class="card-header align-items-center d-flex">
-                <h4 class="card-title mb-0 flex-grow-1">Cascade KPI</h4>
+                <h4 class="card-title mb-0 flex-grow-1">Tupoksi Tw</h4>
+                <div class="flex-shrink-0">
+                </div>
             </div><!-- end card header -->
+
             <div class="card-body">
-                <div id="IndivKPI">
+                <h4 class="card-title mb-0 flex-grow-1">Proker: {{$tupoksiProker->proker}}</h4><br>
+                <div id="tupoksiDepartemen">
                     <div class="row g-4 mb-3">
+
                         <div class="col-sm">
                             <div class="d-flex justify-content-sm-end">
                                 <div class="search-box ms-2">
@@ -19,24 +24,23 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <div class="table-responsive table-card">
-                            <table class="table table-borderless table-centered align-middle table-nowrap mb-0">
-                                <thead class="text-muted table-light ">
-                                    <tr>
+
+                    <div class="table-responsive table-card mt-3 mb-1">
+                        <table class="table align-middle table-nowrap" id="timTable">
+
+                            <thead class="table-light">
+                                <tr>
                                     <th class="sort" data-sort="tw">TW</th>
                                     <th class="sort" data-sort="deskripsi">Deskripsi Proker</th>
                                     <th class="sort" data-sort="progres">Progres Proker</th>
                                     <th class="sort" data-sort="deskripsi">Deskripsi Realisasi</th>
                                     <th class="sort" data-sort="progres">Progres Realisasi</th>
-                                    <th class="sort" data-sort="progres">Komentar</th>
                                     <th class="sort" data-sort="action">Action</th>
-                                    </tr>
-
-                                </thead>
-                                <tbody>
-                                    @if($twCount == 0)
-                                    @for($prok_count=1 ; $prok_count <= 4 ; $prok_count++) <tr>
+                                </tr>
+                            </thead>
+                            <tbody class="list form-check-all">
+                                @if($twCount == 0)
+                                @for($prok_count=1 ; $prok_count <= 4 ; $prok_count++) <tr>
                                     <td>
                                         <div class="d-flex align-items-center">
                                             <div class="flex-grow-1">{{ $prok_count }}</div>
@@ -120,16 +124,11 @@
                                             </div>
                                         </div>
                                     </div>
-                                        @endfor
-                                        @else
-
-                                        @for($prok_count=1 ; $prok_count <= 4 ; $prok_count++) 
-                                        <?php
-                                                                                                $count = 1;
-                                                                                                ?> 
-                                            @foreach($tupoksiTw as $tw) @if($tw->id_proker == $tupoksiProker->id_proker)
-                                            @if($prok_count==$tw->tw)
-                                            <tr>
+                                    @endfor
+                                    @else
+                                    @foreach($tupoksiTw as $tw)
+                                    @if($tw->id_proker == $tupoksiProker->id_proker)
+                                    <tr>
                                         <td>
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-grow-1">{{ $tw->tw }}</div>
@@ -175,7 +174,7 @@
                                                 <div class="add">
                                                     <button class="btn btn-sm btn-success edit-item-btn"
                                                         data-bs-toggle="modal"
-                                                        data-bs-target="#showModal{{$tw->tw}}">Edit Tw</button>
+                                                        data-bs-target="#showModal{{$tw->id_tw}}">Edit Tw</button>
                                                 </div>
                                                 <div class="edit">
                                                     <button type="button"
@@ -187,11 +186,9 @@
                                         </td>
 
                                     </tr>
-                                   
-                                            @foreach($tupoksiRealisasi as $real)
-                                            @if($real->id_tw  == $tw->id)
-                                             <!-- edit Modal -->
-                                            <div class="modal fade" id="showModal{{$tw->tw}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <!-- edit Modal -->
+                                    <div class="modal fade" id="showModal{{$tw->id_tw}}" tabindex="-1"
+                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
                                         <div class="modal-dialog modal-dialog-centered">
                                             <div class="modal-content">
                                                 <div class="modal-header bg-light p-3">
@@ -238,21 +235,10 @@
                                             </div>
                                         </div>
                                     </div>
-                                            @endif
-                                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <?php
-            $count++;
-            ?>
-            @endif
-            @endif
-            @endforeach
-            @if($count == 1) <tr>
-            <td>
+                                    @endif
+                                    @endforeach
+                                    @for($prok_count= 1 + $twCount ; $prok_count <= 4 ; $prok_count++)<tr>
+                                        <td>
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-grow-1">{{ $prok_count }}</div>
                                             </div>
@@ -334,40 +320,83 @@
                                                 </div>
                                             </div>
                                         </div>
-            @endif
-            <?php
-            $count = 0;
-            ?>
-            @endfor
-            @endif
-            </tbody><!-- end tbody -->
+                                        @endfor
+                                        @endif
+                            </tbody>
+                        </table>
+                        <div class="noresult" style="display: none">
+                            <div class="text-center">
+                                <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                                    colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
+                                </lord-icon>
+                                <h5 class="mt-2">Sorry! No Result Found</h5>
+                                <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
+                                    orders for you search.</p>
+                            </div>
+                        </div>
+                    </div>
 
-            </table><!-- end table -->
+                    <div class="d-flex justify-content-end">
+                        <div class="pagination-wrap hstack gap-2">
+                            <a class="page-item pagination-prev disabled" href="#">
+                                Previous
+                            </a>
+                            <ul class="pagination listjs-pagination mb-0"></ul>
+                            <a class="page-item pagination-next" href="#">
+                                Next
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end card -->
+        </div>
+        <!-- end col -->
+    </div>
+    <!-- end col -->
+</div>
+
+<!-- add Modal Realisasi -->
+<div class="modal fade" id="realisasi" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header bg-light p-3">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    id="close-modal"></button>
+            </div>
+            <div class="modal-body">
+                <form method="post" action="{{ route('KPI_TupoksiRealisasi.store') }}" enctype="multipart/form-data"
+                    id="myForm">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="id_tw">Proker</label>
+                        <select name="id_tw" class="form-control" id="id_tw">
+                            @foreach ($tupoksiTw as $tw)
+                            <option value="{{$tw->id_tw}}">{{ "$tw->id_tw" }}</option>
+                            @endforeach
+                        </select>
+
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="progres">Progres </label>
+                        <input type="text" name="progres" class="form-control" id="progres">
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="deskripsi">Deskripsi</label>
+                        <input type="text" name="deskripsi" class="form-control" id="deskripsi">
+
+                    </div>
+                    <div class="modal-footer">
+                        <div class="hstack gap-2 justify-content-end">
+                            <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success" id="add-btn">Add KPI</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div> <!-- .card-->
-</div> <!-- .col-->
-</div> <!-- end row-->
-<div class="d-flex justify-content-end">
-    <div class="pagination-wrap hstack gap-2">
-        <a class="page-item pagination-prev disabled" href="#">
-            Previous
-        </a>
-        <ul class="pagination listjs-pagination mb-0"></ul>
-        <a class="page-item pagination-next" href="#">
-            Next
-        </a>
-    </div>
-</div>
-</div>
-</div><!-- end card -->
-</div>
-<!-- end col -->
-</div>
-<!-- end col -->
-</div>
 
-<!-- add Modal -->
-
-
-@endsection
+    @endsection
